e74dd53d413d870b4d6a193c962a2d9db7bfd4b3
Nursultan <47150499+xumukkg@users.noreply.github.com>
2019-05-20 17:03:12 +0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+ public static int fun(int A){
+     int res = 1;
+     for (int ind = 2; ind<A; ind++) {
+         res = res * ind;
+
+
+     }
+     return res;
+    }
+    public static void main( String[] args) {
+	// write your code here
+
+            int temp = fun(3);
+            System.out.println(temp);
+
+    }
+}
