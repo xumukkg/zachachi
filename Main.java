16331b61685d312be90e076f6b5272c81951df96
Nursultan <47150499+xumukkg@users.noreply.github.com>
2019-05-20 18:18:24 +0600

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,30 +1,52 @@
 package com.company;
-// решения это задачи нужно демонстрация того, как себя будет вести себя функция в определенных языках программирования
-// для этого я испоьзовал Java 
-//        задача выглядит след., образом !!
-// Функция Ф (А)
-//   Рез = 1;
-//   Для Инд = 2 По А Цикл
-//     Рез = Рез * Инд;
-//   КонецЦикла;
-//   Возврат Рез;
-//  КонецФункции
-
-public class Main {
- public static int fun(int A){
-     int res = 1;
-     for (int ind = 2; ind<A; ind++) {
-         res = res * ind;
 
 
-     }
-     return res;
-    }
-    public static void main( String[] args) {
-	// write your code here
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int temp;
+        int A [] = {2, 3,4,5,6,7,8,9};
+//        int B [10,8,6,4,2];
 
-            int temp = fun(3);
-            System.out.println(temp);
+        for (int i = 0; i < A.length; i++)
+        {
+            for (int j = i + 1; j < A.length; j++)
+            {
+                if (A[i] < A[j])
+                {
+                    temp = A[i];
+                    A[i] = A[j];
+                    A[j] = temp;
+                }
+            }
+        }
+        System.out.print("Descending Order:");
+        for (int i = 0; i < A.length - 1; i++)
+        {
+            System.out.print(A[i] + ",");
+        }
+        System.out.print(A[A.length - 1]);
+               int B []={2,4,6,8,10,12};
+
+        for (int i = 0; i < B.length; i++)
+        {
+            for (int j = i + 1; j < B.length; j++)
+            {
+                if (B[i] < B[j])
+                {
+                    temp = B[i];
+                    B[i] = B[j];
+                    B[j] = temp;
+                }
+            }
+        }
+        System.out.println("Descending Order:");
+        for (int i = 0; i < B.length - 1; i++)
+        {
+            System.out.print(B[i] + ",");
+        }
+        System.out.println(B[B.length - 1]);
 
     }
 }
