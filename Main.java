fd2fd6a93d3a4e48d279267ee19ad494228c1474
Nursultan <47150499+xumukkg@users.noreply.github.com>
2019-05-21 15:40:56 +0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 package com.company;
 // для выполнения этой задачи нужно демнострация функции на каком нибудь языке программирования
-// для жтой цели использована Java
+// для этой цели использована Java
 public class Main {
     public static int fun(int A){
         int res = 1;
